66ee8960b25307afbdb13f2c3169762dcfe4c3bd
Paul Bailey <paul.m.bailey@gmail.com>
2015-12-30 17:25:42 -0500

    added nb-blog

--- js/app.js
+++ js/app.js
@@ -6,8 +6,7 @@ function img (path) {
   return 'img/' + path;
 }
 
-var napp = angular.module('NeutronApp', [
-  'ngMaterial', 'ngRoute', 'angularMoment', 'hc.commonmark']);
+var napp = angular.module('NeutronApp', ['nb.blog', 'ngMaterial', 'ngRoute', 'angularMoment']);
 
 napp.config(function ($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
@@ -16,9 +15,9 @@ napp.config(function ($routeProvider, $locationProvider) {
 napp.config(function ($routeProvider, $mdThemingProvider) {
   $routeProvider
     .when('/',
-      {controller:'ListController', templateUrl: tpl('list.html')})
+      {controller:'nbBlogListController', templateUrl: tpl('list.html')})
     .when('/blog/post/:year/:slug',
-      {controller:'PostController', templateUrl: tpl('post.html')});
+      {controller:'nbBlogPostController', templateUrl: tpl('post.html')});
       
   $mdThemingProvider.theme('default')
     .primaryPalette('amber')
@@ -27,4 +26,34 @@ napp.config(function ($routeProvider, $mdThemingProvider) {
 
 napp.run(function ($rootScope) {
   
+});
+
+napp.controller('GlobalController', function ($scope, $mdDialog) {
+  $scope.tpl = tpl;
+  $scope.img = img;
+  var title = "Neutron Drive Blog";
+  
+  $scope.set_title = function (t) {
+    if (t) {
+      $scope.title = t + " | " + title;
+    }
+    
+    else {
+      $scope.title = title;
+    }
+  };
+  
+  $scope.show_error = function (message, event) {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .clickOutsideToClose(true)
+        .title('Error!')
+        .textContent(message)
+        .ariaLabel('Error')
+        .ok('OK')
+        .targetEvent(event)
+    );
+  };
+  
+  $scope.set_title();
 });
